c83792e203b3ce7d1ce2f89177605ace86cb3834
Bachar Wehbi <me@bachwehbi.net>
2014-05-07 17:44:11 +0200

    Addition of custom exceptions and migrating to requests instead of http.client

--- lib/bbt.py
+++ lib/bbt.py
@@ -9,7 +9,7 @@ import json
 import hmac
 import base64
 import hashlib
-import http.client
+import requests
 import urllib.parse
 from email import utils
 
@@ -35,43 +35,90 @@ class BBT:
     stringToSign = "%s\n%s\n%s\n%s\n%s" % (verb, c_md5, c_type, date, uri)
     return self.sign(stringToSign)
 
-  def __postData__(self, uri, body, auth = True):
-    # Set cURL options
-    if self.ssl:
-      conn = http.client.HTTPSConnection( self.hostname, self.port )
+  def __processResponse__(self, response):
+    code    = response['status']
+    body   = json.loads(response['body'])
+    if code < 400:
+      return body
     else:
-      conn = http.client.HTTPConnection( self.hostname, self.port )
+      errcode = body['error']['code']
+      errmsg  = body['error']['message']
+      if code == 400:
+        if errcode == 1101:
+          raise AuthenticationError("Status: 400; Code 1101; Message: %s" % errmsg)
+        elif errcode == 1401:
+          raise ParameterError("Status: 400; Code 1401; Message: %s" % errmsg)
+        elif errcode == 1403:
+          raise BadRequestError("Status: 400; Code 1403; Message: %s" % errmsg)
+        elif errcode == 1404:
+          raise TypeError("Status: 400; Code 1404; Message: %s" % errmsg)
+        elif errcode == 1405:
+          raise BadTypeError("Status: 400; Code 1405; Message: %s" % errmsg)
+        elif errcode == 1406:
+          raise PayloadLimitError("Status: 400; Code 1406; Message: %s" % errmsg)
+        else:
+          raise UnexpectedError("Status: %s; Code %s; Message: %s" % (code, errcode, errmsg) )
+      elif code == 405:
+        if errcode == 1102:
+          raise NotAllowedError("Status: 405; Code 1102; Message: %s" % errmsg)
+        else:
+          raise UnexpectedError("Status: %s; Code %s; Message: %s" % (code, errcode, errmsg) )
+      elif code == 500:
+        if errcode == 1201:
+          raise InternalError("Status: 500; Code 1201; Message: %s" % errmsg)
+        else:
+          raise InternalError("Status: %s; Code %s; Message: %s" % (code, errcode, errmsg) )
+      elif code == 404:
+        if errcode == 1301:
+          raise NotFoundError("Status: 404; Code 1301; Message: %s" % errmsg)
+        if errcode == 1302:
+          raise NotFoundError("Status: 404; Code 1302; Message: %s" % errmsg)
+        if errcode == 1303:
+          raise NotFoundError("Status: 404; Code 1303; Message: %s" % errmsg)
+        if errcode == 1304:
+          raise AlreadyExistError("Status: 404; Code 1304; Message: %s" % errmsg)
+        if errcode == 1305:
+          raise AlreadyExistError("Status: 404; Code 1305; Message: %s" % errmsg)
+        if errcode == 1306:
+          raise AlreadyExistError("Status: 404; Code 1306; Message: %s" % errmsg)
+        else:
+          raise UnexpectedError("Status: %s; Code %s; Message: %s" % (code, errcode, errmsg) )
+      else:
+        raise UnexpectedError("Status: %s; Code %s; Message: %s" % (code, errcode, errmsg) )
+
+  def __postData__(self, uri, body, auth = True):
+    if self.ssl:
+      url = "%s://%s:%s%s" % ( 'https', self.hostname, self.port, uri )
+    else:
+      url = "%s://%s:%s%s" % ( 'http', self.hostname, self.port, uri )
 
     md5 = bytes.decode( base64.b64encode( hashlib.md5( str.encode( body ) ).digest() ) )
     date = utils.formatdate()
     if auth:
-      sig = self.__signRequest__("POST", uri, date, "application/json", md5)
-      conn.request('POST', uri, body, { 'Content-MD5': md5, 'Content-Type': 'application/json', 'Date': date, 'Authorization': sig } )
+      sig = self.__signRequest__('POST', uri, date, "application/json", md5)
+      headers = { 'Content-MD5': md5, 'Content-Type': 'application/json', 'Date': date, 'Authorization': sig } 
     else:
-      conn.request('POST', uri, body, { 'Content-MD5': md5, 'Content-Type': 'application/json', 'Date': date } )
-    
-    resp = conn.getresponse()
-    return { 'status': resp.status, 'body': resp.read() }
+      headers = { 'Content-MD5': md5, 'Content-Type': 'application/json', 'Date': date } 
+
+    r = requests.post( url, data=body, headers=headers )
+    return self.__processResponse__( { 'status': r.status_code, 'body': r.text } )
 
   def __getData__(self, uri, query, auth = True):
-    # Set cURL options
     if self.ssl:
-      conn = http.client.HTTPSConnection( self.hostname, self.port )
+      url = "%s://%s:%s%s" % ( 'https', self.hostname, self.port, uri )
     else:
-      conn = http.client.HTTPConnection( self.hostname, self.port )
+      url = "%s://%s:%s%s" % ( 'http', self.hostname, self.port, uri )
 
     full_uri = "%s?%s" % ( uri, urllib.parse.urlencode( query ) )
-    
     date = utils.formatdate()
-    
     if auth:
       sig = self.__signRequest__('GET', full_uri, date, "application/json")
-      conn.request('GET', full_uri, '', { 'Content-Type': 'application/json', 'Date': date, 'Authorization': sig } )
+      headers = { 'Content-Type': 'application/json', 'Date': date, 'Authorization': sig } 
     else:
-      conn.request('GET', full_uri, '', { 'Content-Type': 'application/json', 'Date': date } )
-    
-    resp = conn.getresponse()
-    return { 'status': resp.status, 'body': resp.read() }
+      headers = { 'Content-Type': 'application/json', 'Date': date } 
+
+    r = requests.get( url, params=query, headers=headers )
+    return self.__processResponse__( { 'status': r.status_code, 'body': r.text } )
 
   def publicRead(self, owner, device, service, resource, limit = 1, source = "live", metric = "avg" ):
     query = {'owner': owner, 'device': device, 'service': service, 'resource': resource, 'limit': limit, 'source': source, 'metric': metric}
@@ -118,14 +165,35 @@ class BBT:
 class AuthenticationError(Exception):
     pass
 
+class InternalError(Exception):
+    pass
+
 class NotFoundError(Exception):
     pass
 
+class AlreadyExistError(Exception):
+    pass
+
+class NotAllowedError(Exception):
+    pass
+
 class UsageLimitError(Exception):
+    pass
+
+class PayloadLimitError(Exception):
     pass
 
 class UnexpectedError(Exception):
     pass
 
 class BadRequestError(Exception):
+    pass
+
+class ParameterError(Exception):
+    pass
+
+class TypeError(Exception):
+    pass
+
+class BadTypeError(Exception):
     pass
