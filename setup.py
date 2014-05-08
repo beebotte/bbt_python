ef352108f062a4a1eb4ebf196386f60c9e6c6032
Bachar Wehbi <me@bachwehbi.net>
2014-05-08 11:48:42 +0200

    Addition of pip package setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='beebotte',
+    version='0.1.0',
+    description='Python library for interfacing with Beebotte',
+    url='http://beebotte.com',
+    author='Beebotte',
+    author_email='bwehbi@beebotte.com',
+    packages=['beebotte'],
+    keywords='beebotte, api, rest, pubsub, restful, iot, internet of things, device, realtime, connected, websockets',
+    license='MIT',
+)
